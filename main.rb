3f69586e8c88acb20994d964170540acc72f687f
Vladimir <vladimir.gumennyy@ooma.com>
2021-02-16 19:50:05 -0800

    2021.02.16

--- main.rb
+++ main.rb
@@ -1,17 +1,31 @@
-def mapping(letters)
-  letters.inject({}) do |h, char|
-    h[char] = char.upcase
-    h
+def to_star_shorthand(str)
+  prev = str[0]
+  cnt = 1
+  res = ''
+  char = ''
+  (1...str.size).each do |i|
+    char = str[i]
+    if char == prev
+      cnt += 1
+    else
+      if cnt == 1
+        res << prev
+      else
+        res << "#{prev}*#{cnt}"
+        cnt = 1
+      end
+      prev = char
+    end
   end
 
-
-  # h = {}
-  # letters.each do |char|
-  #   h[char] = char.upcase
-  # end
-  # h
+  if cnt == 1
+    res << char
+  else
+    res << "#{char}*#{cnt}"
+  end
 end
 
-p mapping(["p", "s"]) == { "p" => "P", "s" => "S" }
-p mapping(["a", "b", "c"]) == { "a" => "A", "b" => "B", "c" => "C" }
-p mapping(["a", "v", "y", "z"]) == { "a" => "A", "v" => "V", "y" => "Y", "z" => "Z" }
+p to_star_shorthand("abbccc") == "ab*2c*3"
+p to_star_shorthand("77777geff") == "7*5gef*2"
+p to_star_shorthand("abc") == "abc"
+p to_star_shorthand("") == ""
